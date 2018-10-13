a6325a5bc38be3df7fea97967379fee545ba1ee1
Afsal Thaj <afsal.taj06@gmail.com>
2018-10-13 13:29:18 +1100

    Remove wrong comments

--- src/main/scala/com/scalaz/config/Config.scala
+++ src/main/scala/com/scalaz/config/Config.scala
@@ -11,11 +11,9 @@ object Config {
   type MapReader[A] = Kleisli[NonEmptyList[ConfigError] \/ ?, Env, A]
   type MapWriter[A] = A => Map[String, String]
 
-  // ConfigSyntax should be derived if there is a ConfigSyntax available for each individual fields in  A.
   def reader[A](config: Config[MapReader, A]): MapReader[A] =
     config.apply
 
-  // ConfigSyntax should be derived if there is a ConfigSyntax available for each individual fields in A for a Mapwriter.
   def writer[A](config: Config[MapWriter, A]): MapWriter[A] =
     config.apply
 }
