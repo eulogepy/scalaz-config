55cfd54a83ae8ab9576f2271c02d1151c4fffd17
Afsal Thaj <afsal.taj06@gmail.com>
2019-02-16 23:10:29 +1100

    Make the examples more abstract

--- src/main/scala/com/scalaz/config/examples/CoproductExample.scala
+++ src/main/scala/com/scalaz/config/examples/CoproductExample.scala
@@ -12,7 +12,7 @@ object CoproductExample extends App {
   case class SampleConfig(v1: String, v2: String)
   case class AnotherConfig(v1: String, v2: Int, v3: Double)
 
-  def sampleConfig[F[_]]: Config[F, SampleConfig] = new Config[F, SampleConfig] {
+  def firstConfig[F[_]]: Config[F, SampleConfig] = new Config[F, SampleConfig] {
     override def apply(implicit F: ConfigSyntax[F]): F[SampleConfig] =
       (read[F, String]("x1") |@|
         read[F, String]("x2")) {
@@ -20,15 +20,15 @@ object CoproductExample extends App {
       }
   }
 
-  def anotherConfig[F[_]]: Config[F, AnotherConfig] = new Config[F, AnotherConfig] {
+  def secondConfig[F[_]]: Config[F, AnotherConfig] = new Config[F, AnotherConfig] {
     override def apply(implicit F: ConfigSyntax[F]): F[AnotherConfig] =
       (read[F, String]("x3") |@| read[F, Int]("x4") |@| read[F, Double]("x5")) {
         AnotherConfig
       }
   }
 
-  val mapReader: MapReader[SampleConfig \/ AnotherConfig] =
-    sampleConfig[MapReader].apply.or(anotherConfig[MapReader].apply)
+  def finalReader[F[_]: ConfigSyntax]: F[SampleConfig \/ AnotherConfig] =
+    firstConfig[F].apply.or(secondConfig[F].apply)
 
   // Only variables for left config exists in Env (Ex: use connector1)
   val validConfigForSampleConfig =
@@ -40,7 +40,9 @@ object CoproductExample extends App {
 
   assert(
     // A right of coproduct or a left of nonemptylist of errors.
-    mapReader(validConfigForSampleConfig) == Success(-\/(SampleConfig("v1", "v2")))
+    finalReader[MapReader].apply(validConfigForSampleConfig) == Success(
+      -\/(SampleConfig("v1", "v2"))
+    )
   )
 
   // Only variables for left config exists in Env (Ex: use connector2 in the absence of connector1)
@@ -53,7 +55,9 @@ object CoproductExample extends App {
     )
 
   assert(
-    mapReader(validConfigForAnotherConfig) == Success(\/-(AnotherConfig("v3", 1, 2.0)))
+    finalReader[MapReader].apply(validConfigForAnotherConfig) == Success(
+      \/-(AnotherConfig("v3", 1, 2.0))
+    )
   )
 
   val invalidConfig =
@@ -65,7 +69,7 @@ object CoproductExample extends App {
     )
 
   assert(
-    mapReader(invalidConfig) == Failure(
+    finalReader[MapReader].apply(invalidConfig) == Failure(
       NonEmptyList(
         ConfigError("x1", ConfigError.MissingValue),
         ConfigError(
@@ -87,7 +91,7 @@ object CoproductExample extends App {
     )
 
   assert(
-    mapReader(allConfigsExist) ==
+    finalReader[MapReader].apply(allConfigsExist) ==
       Success(-\/(SampleConfig("v1", "v2")))
   )
 }
